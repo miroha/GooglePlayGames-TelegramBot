f5130826277c5c90bed55ccb32a042585ab8e37f
Pavel Mironov <muposhaaa@gmail.com>
2020-11-09 22:28:40 +0300

    Replace application.properties with yaml configuration

--- src/main/java/ru/miroha/GooglePlayGamesTelegramBotApp.java
+++ src/main/java/ru/miroha/GooglePlayGamesTelegramBotApp.java
@@ -3,12 +3,13 @@ package ru.miroha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 /**
  * Allows to register bot in spring context automatically and also use it as standard spring bean.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GooglePlayGamesTelegramBotApp {
 
     public static void main(String[] args) {
